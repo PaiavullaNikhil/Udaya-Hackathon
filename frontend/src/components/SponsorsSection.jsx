47aadbab402721e0d5d12552f8e27c7c8110bf3e
Rishben <nikhil.20th65@gmail.com>
2025-09-09 16:19:33 +0530

    Balance Changes

--- frontend/src/components/SponsorsSection.jsx
+++ frontend/src/components/SponsorsSection.jsx
@@ -1,6 +1,6 @@
 // eslint-disable-next-line no-unused-vars
 import { motion } from 'framer-motion';
-import Sponsors from '../constants/Sponsors';
+import Sponsors from '../constants/sponsors';
 
 const SponsorsSection = () => {
   return (
