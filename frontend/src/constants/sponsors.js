5132de9014da0cd70d28a304496b997b6403c05e
Rishben <nikhil.20th65@gmail.com>
2025-09-09 17:17:32 +0530

    add sponsors

--- frontend/src/constants/sponsors.js
+++ frontend/src/constants/sponsors.js
@@ -1,15 +1,20 @@
-const Sponsors=[
-    {
-        heading:"Main Sponsor",
-        name:"Gyankool Research Labs",
-        desc:"Empowering innovation through cutting-edge research",
-    },
-      // you can add more sponsors like this:
-//   {
-//     heading: "Co-Sponsor",
-//     name: "Tech Corp",
-//     desc: "Building the future with technology",
-//   }
-]
+const Sponsors = [
+  {
+    heading: "Main Sponsor",
+    name: "Derbi Foundations",
+    desc: "Fueling dreams of innovators, entrepreneurs and startups",
+  },
+  {
+    heading: "Co Sponsor",
+    name: "Gyankool Research Labs",
+    desc: "Empowering innovation through cutting-edge research",
+  },
+  // you can add more sponsors like this:
+  //   {
+  //     heading: "Co-Sponsor",
+  //     name: "Tech Corp",
+  //     desc: "Building the future with technology",
+  //   }
+];
 
-export default Sponsors
+export default Sponsors;
