da37117929099c3944bdbcd16e9232a1de6d15a2
Rishben <nikhil.20th65@gmail.com>
2025-09-13 17:07:24 +0530

    winner count

--- frontend/src/constants/stats.js
+++ frontend/src/constants/stats.js
@@ -1,8 +1,9 @@
-  const stats = [
-    { number: 12, suffix: " Hours", label: "Of Innovation" },
-    { number: 5, suffix: " Themes", label: "To Choose From" },
-    { number: 100, suffix: "+ Teams", label: "Participants Expected" },
-    { number: 10, suffix: " Winner", label: "Will Be Crowned" }
-  ];
+const stats = [
+  { number: 12, suffix: " Hours", label: "Of Innovation" },
+  { number: 5, suffix: " Themes", label: "To Choose From" },
+  { number: 100, suffix: "+ Teams", label: "Participants Expected" },
+  { number: 5, suffix: " Winners", label: "Will Be Crowned" },
+  { number: 5, suffix: " Runner-ups", label: "Will Be Recognized" },
+];
 
-  export default stats
+export default stats;
