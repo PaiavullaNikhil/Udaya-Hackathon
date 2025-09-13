4be567feade37e8e71330b6c8be5c0dedb7a9eb4
Rishben <nikhil.20th65@gmail.com>
2025-09-13 16:15:56 +0530

    CORS error

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ dotenv.config();
 const app = express();
 
 // Middleware
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: process.env.FRONTEND_URI, credentials: true }));
 app.use(express.json()); // for JSON
 app.use(bodyParser.urlencoded({ extended: true })); // for URL-encoded
 
